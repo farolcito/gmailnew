1b3bfabf536abadd639417b7432e39c9027ce550
farolcito <141853922+farolcito@users.noreply.github.com>
2023-10-08 20:49:21 -0300

    cambiosmail

--- src/test/java/GestorCorreo/SendMailsTest.java
+++ src/test/java/GestorCorreo/SendMailsTest.java
@@ -12,14 +12,18 @@ public class SendMailsTest {
     User gonza= app.crearUsuario("Gonzalo", "Chaco", "gonzachaco");
     User oriana = app.crearUsuario("Oriana", "Farela", "orianafarela");
     User mario = app.crearUsuario("Mario", "Gonzales", "mariogonzales@gmail.com");
+    User valen = app.crearUsuario("Valentina", "Salmon", "valensalmon@gmail.com");
+
     ArrayList<String> para = new ArrayList<>();
     para.add("orianafarela@aquilita.com");
     para.add("mariogonzales@gmail.com");
+    para.add("valensalmon@gmail.com");    
 
     gonza.crearMensaje(app, "Buen dia", "Hola queres jugar", para);
     assertEquals(1, gonza.getSalida().tamano());
     assertEquals(1, oriana.getEntrada().tamano());
     assertEquals(1, mario.getEntrada().tamano());
+    assertEquals(1, valen.getEntrada().tamano());
 }
 
 }
