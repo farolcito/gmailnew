18a67e3bd7d0c430f8e3b05620390985d37187ad
farolcito <141853922+farolcito@users.noreply.github.com>
2023-10-08 19:50:00 -0300

    bandeja

--- src/test/java/GestorCorreo/SendMailsTest.java
+++ src/test/java/GestorCorreo/SendMailsTest.java
@@ -8,15 +8,15 @@ import java.util.ArrayList;
 public class SendMailsTest {
 @Test
     public void crateUserandMail(){
-    MailManager aplicacion = new MailManager();
-    User gonza= aplicacion.crearUsuario("Gonzalo", "Chaco", "gonzachaco");
-    User oriana = aplicacion.crearUsuario("Oriana", "Farela", "orianafarela");
-    User mario = aplicacion.crearUsuario("Mario", "Gonzales", "mariogonzales@gmail.com");
+    MailManager app = new MailManager();
+    User gonza= app.crearUsuario("Gonzalo", "Chaco", "gonzachaco");
+    User oriana = app.crearUsuario("Oriana", "Farela", "orianafarela");
+    User mario = app.crearUsuario("Mario", "Gonzales", "mariogonzales@gmail.com");
     ArrayList<String> para = new ArrayList<>();
     para.add("orianafarela@aquilita.com");
     para.add("mariogonzales@gmail.com");
 
-    gonza.crearMensaje(aplicacion, "Buen dia", "Hola queres jugar", para);
+    gonza.crearMensaje(app, "Buen dia", "Hola queres jugar", para);
     assertEquals(1, gonza.getSalida().tamano());
     assertEquals(1, oriana.getEntrada().tamano());
     assertEquals(1, mario.getEntrada().tamano());
